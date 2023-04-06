6e2bbd88077829e8d8fe75521f7fd5deb4219fbf
BreakTos <76687690+BreakTos@users.noreply.github.com>
2023-04-06 16:40:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var name1,name2;
 function roll(Buttt){
   var But1=document.getElementById(Buttt);
   var num=Math.floor((Math.random()*10))%6+1;
@@ -14,8 +15,22 @@ function declareWinner(button1 , button2){
     const buttonNumber1 = buttonText1.match(/\d+/)[0];
     const buttonText2 = but2.textContent;
     const buttonNumber2 = buttonText2.match(/\d+/)[0];
-    if(buttonNumber1>buttonNumber2) alert("Player1 is Right");
-    if(buttonNumber1<buttonNumber2) alert("Player2 is Right");
+    if(buttonNumber1>buttonNumber2) alert(name1+" is Right");
+    if(buttonNumber1<buttonNumber2) alert(name2+" is Right");
     if(buttonNumber1==buttonNumber2) alert("Nobody wins , Start dice fight again");
-  
+}
+function handleSubmit(event,formid){
+  event.preventDefault();
+  if(formid=="myForm1")
+   name1=document.getElementById("name1").value;
+  else
+   name2=document.getElementById("name2").value;
+  event.target.reset();
+}
+function putVal(button1 , button2){
+  var but1=document.getElementById("button1");
+  but1.innerHTML = name1;
+  var but2=document.getElementById("button2");
+  but2.innerHTML = name2;
+ 
 }
